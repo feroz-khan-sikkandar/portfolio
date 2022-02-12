caa69e15545fff08bc00a687675bfd47875cde5d
Feroz Khan <feroz.khan.sikkandar@gmail.com>
2022-02-12 18:34:07 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// Download CV on Button Click
+document.querySelector('#download-cv').addEventListener('click', downloadCv);
+
+function downloadCv () {
+    let fileName = 'Feroz_Khan.pdf'
+    //Set the File URL.
+    var url = "files/" + fileName;
+
+    //Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/octetstream" });
+
+        //Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send(); 
+}
+
+// Smooth Scrolling
+ $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800
+      );
+    } // End if
+  });
